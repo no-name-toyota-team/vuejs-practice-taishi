de7c7cea0ff86ce66639392546097c4342937be9
Taishi Naka <taishi.develop@gmail.com>
2021-04-15 01:36:07 +0900

    create see or hide state component

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,32 @@
 <template>
   <div id="app">
     <WelcomeMessage :msg="msg"/>
-    
+    <show-hide :canSee="canSee"></show-hide>
+    <button @click="handleChangeButton">{{ !canSee ? "Show":"Hide" }}</button>
+
   </div>
 </template>
 
 <script>
   import WelcomeMessage from './components/WelcomeMessage'
+  import ShowHide from './components/ShowHide'
 
   export default {
     name: 'App',
     data(){
       return{
-        msg: 'Welcome, here'
+        msg: 'Welcome, here',
+        canSee: false,
+      }
+    },
+    methods:{
+      handleChangeButton:()=>{
+        console.log(this.canSee)
       }
     },
     components: {
       WelcomeMessage,
+      ShowHide,
     }
   }
 </script>
---
+++ src/components/ShowHide.vue
@@ -0,0 +1,15 @@
+<template>
+    <div>
+        <p v-if="canSee">You can see this!</p>
+        <p v-else>You cannot see this...</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name: "ShowHide",
+    props: {
+        canSee: Boolean,
+    }
+}
+</script>
